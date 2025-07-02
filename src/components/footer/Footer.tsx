e2e375d8cbbb518e1d922c8578d02acbca492c0d
Wesley Lima <contato.wezzlim@outlook.com>
2025-07-02 15:54:37 -0300

    :sparkles: Construção Footer e Navbar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,34 @@
+import { GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react"
+
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="
+        bg-orange-300
+        py-6
+        md:py-9
+        lg:py-12
+        text-center">
+        <p className="
+        text-sm
+        text-gray-600">
+        © 2025 Feito por Cavaleiros Templários SA. Todos os direitos reservados.
+        </p>
+        <div className="
+            flex 
+            justify-center 
+            gap-6 mt-4">
+            <a href="" target="_blank"> 
+              <LinkedinLogo size={32} color="#01398e" /> 
+            </a>
+            <a href="https://github.com/Cavaleiros-Templarios/" target="_blank">
+              <GithubLogo size={32} color="#01398e" />
+            </a>
+            <a href="" target="_blank">
+              <InstagramLogo size={32} color="#01398e" />
+            </a>
+        </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,83 @@
+import { MagnifyingGlassIcon, ShoppingCartIcon, UserIcon } from "@phosphor-icons/react"
+import { useState, type ChangeEvent, type FormEvent } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+
+	const navigate = useNavigate()
+
+	const [nome, setNome] = useState<string>("")
+
+	function handleBuscarProdutos(e: ChangeEvent<HTMLInputElement>){
+		setNome(e.target.value)
+	}
+
+	function buscarProdutos(e: FormEvent<HTMLFormElement>){
+		e.preventDefault()
+		navigate(`/consultarnome/${nome}`)
+		setNome('')
+	}
+	
+	return (
+		<>
+			<div className="flex justify-center w-full py-4 text-white bg-orange-300">
+				<div className="container flex items-center justify-between mx-4 text-lg">
+					<Link to="/home">
+						<img
+							src="https://i.imgur.com/KD8nz0z.png"
+							alt="Logo"
+							className="w-30"
+						/>
+					</Link>
+
+					<div className="relative flex items-center justify-center w-2/5 text-black">
+						<form 
+							className="flex items-center justify-center w-full"
+							onSubmit={buscarProdutos}
+						>
+							<input
+								className="w-10/12 px-4 py-4 bg-white rounded-lg h-9 focus:outline-none"
+								type="search"
+								placeholder="Procurar"
+								id="nome"
+								name="nome"
+								required
+								value={nome}
+								onChange={(e: ChangeEvent<HTMLInputElement>) => handleBuscarProdutos(e)}
+							/>
+							<button
+								type="submit"
+								className="h-9 w-9 p-2.5 ms-2 text-sm font-medium text-white bg-orange-300 hover:bg-teal-900 rounded-lg border border-teal-700"
+							>
+								<MagnifyingGlassIcon
+									size={14}
+									weight="bold"
+								/>
+							</button>
+						</form>
+					</div>
+
+					<div className="flex items-center gap-4 py-4">
+						<Link
+							to="/sobre"
+							className="hover:underline"
+						>
+							Sobre
+						</Link>
+							<UserIcon
+								size={32}
+								weight="bold"
+							/>
+
+							<ShoppingCartIcon
+								size={32}
+								weight="bold"
+							/>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Navbar
